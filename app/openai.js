0da6530b39e6333171ac2dd41192498c88785973
Torean Joel <toreanjoel@Toreans-MacBook-Pro.local>
2023-10-22 14:25:59 +0200

    chore: update model system prompt

--- app/openai.js
+++ app/openai.js
@@ -27,7 +27,7 @@ export async function computeCourses(data) {
     const messages = [
       {
         role: "system",
-        content: "Given a list of academic courses, complete with each course's index and name, generate a JSON response that fills in potential career options for each listed course. For each course, provide about 20 career options. The career options should start with capital letters and be separated by a comma and a space. If a course is a duplicate of an earlier course (based on its content), provide the same career options and include a \"Duplicate\" key with the value of the original course's index. If there is no duplicate, the \"Duplicate\" key should have an empty string as its value. If any of the course The JSON response should be clean and contain no explanatory or verbose text outside of the JSON structure itself.\n",
+        content: "Given a list of academic courses / Job Career titles, complete with each index and name, generate a JSON response that fills in potential career options or alternative options for each listed course/job. For each course/job, provide about 20 career options. The career options should start with capital letters and be separated by a comma and a space. If a course/job is a duplicate of an earlier course/job (based on its content), provide the same career options and include a \"Duplicate\" key with the value of the original course's index. If there is no duplicate, the \"Duplicate\" key should have an empty string as its value. If any of the course The JSON response should be clean and contain no explanatory or verbose text outside of the JSON structure itself.\n",
       },
       {
         role: "user",
